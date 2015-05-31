05d4199307ed4891df75c833c4ae1cf4f0839a56
Lukáš Kvídera <kvideral@students.zcu.cz>
2015-05-31 13:50:57 +0200

    ClassLoader implementation

--- cachesim2/src/main/java/cz/zcu/kiv/cacheSimulator/ClassLoader.java
+++ cachesim2/src/main/java/cz/zcu/kiv/cacheSimulator/ClassLoader.java
@@ -1,7 +1,11 @@
 package cz.zcu.kiv.cacheSimulator;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -9,6 +13,8 @@ import java.util.logging.Logger;
 
 import cz.zcu.kiv.cacheSimulator.cachePolicies.ICache;
 import cz.zcu.kiv.cacheSimulator.consistency.IConsistencySimulation;
+import cz.zcu.kiv.cacheSimulator.gui.MainGUI;
+import cz.zcu.kiv.cacheSimulator.shared.GlobalMethods;
 
 /**
  * ClassLoader.java
@@ -19,7 +25,9 @@ import cz.zcu.kiv.cacheSimulator.consistency.IConsistencySimulation;
  */
 public class ClassLoader {
 
-  private final static Logger LOGGER =Logger.getLogger(ClassLoader.class.getName());
+  private final static Logger LOGGER = Logger.getLogger(ClassLoader.class.getName());
+
+
   /**
    * metoda pro nacteni cache policies z adresare
    *
@@ -27,7 +35,7 @@ public class ClassLoader {
    *          cesta
    */
   public static List<String> loadClassInfo(final String path, final String packageName) {
-    final List<String> classInfo = new ArrayList<String>();
+    final List<String> classInfo = new ArrayList<>();
     final String pathToPackage = (path + packageName);
     final File dir = new File(pathToPackage);
     final File[] files = dir.listFiles();
@@ -66,6 +74,62 @@ public class ClassLoader {
         } catch (final Exception e) {
           LOGGER.log(Level.SEVERE, null, e);
         }
+      }
+    }
+    return classInfo;
+  }
+
+
+  /**
+   * metoda pro nacteni cache simulatoru z jar souboru
+   *
+   * @param path
+   *          cesta k jar souboru
+   */
+  public static List<String> loadClassInfoFromJar(final String path, final String packageName) {
+    final List<String> classNames = GlobalMethods.getJarClassNames(path, packageName);
+    final List<String> classInfo = new ArrayList<>();
+    for (final String className : classNames) {
+      if (className.contains("ICache") || className.contains("IConsistency")
+          || className.contains("Data")) {
+        continue;
+      }
+      try {
+        final URL url = new URL("jar:file:/" + path + "!/");
+        try (final URLClassLoader ucl = new URLClassLoader(new URL[] {url})) {
+          final Class<?> myClass = Class.forName(className.replace("/", "."), true, ucl);
+          Object newObject = null;
+          // chceme pouze tridy s konstruktory bez parametru
+          final Constructor<?>[] cons = myClass.getConstructors();
+          if (cons.length == 1 && cons[0].getParameterTypes().length == 0) {
+            newObject = myClass.newInstance();
+          } else {
+            continue;
+          }
+
+          newObject = myClass.newInstance();
+          if (newObject instanceof ICache) {
+            classInfo.add(((ICache) newObject).cacheInfo());
+          }
+          if (newObject instanceof IConsistencySimulation) {
+            classInfo.add(((IConsistencySimulation) newObject).getInfo());
+          }
+          if (className.contains("Panel")) {
+            classInfo.add(myClass.getName());
+          }
+        }
+      } catch (final InstantiationException ex) {
+        Logger.getLogger(MainGUI.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (final IllegalAccessException ex) {
+        Logger.getLogger(MainGUI.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (final MalformedURLException ex) {
+        ex.printStackTrace();
+        Logger.getLogger(MainGUI.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (final ClassNotFoundException ex) {
+        Logger.getLogger(MainGUI.class.getName()).log(Level.SEVERE, null, ex);
+        ex.printStackTrace();
+      } catch (final IOException ex) {
+        Logger.getLogger(MainGUI.class.getName()).log(Level.SEVERE, null, ex);
       }
     }
     return classInfo;
