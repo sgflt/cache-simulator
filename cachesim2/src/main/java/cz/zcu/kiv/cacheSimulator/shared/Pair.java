85fb774f7961c733a56458074d8246d42233f236
Lukáš Kvídera <kvideral@students.zcu.cz>
2015-05-31 13:54:12 +0200

    Fixed raw type warning

--- cachesim2/src/main/java/cz/zcu/kiv/cacheSimulator/shared/Pair.java
+++ cachesim2/src/main/java/cz/zcu/kiv/cacheSimulator/shared/Pair.java
@@ -10,70 +10,71 @@ package cz.zcu.kiv.cacheSimulator.shared;
  */
 public class Pair<A, B> {
 
-	private A first;
+  private A first;
 
-	private B second;
+  private B second;
 
-	public Pair(A first, B second) {
-		super();
-		this.first = first;
-		this.second = second;
-	}
+  public Pair(final A first, final B second) {
+    super();
+    this.first = first;
+    this.second = second;
+  }
 
-	public String toString() {
-		return "(" + first + ", " + second + ")";
-	}
+  @Override
+  public String toString() {
+    return "(" + this.first + ", " + this.second + ")";
+  }
 
-	public A getFirst() {
-		return first;
-	}
+  public A getFirst() {
+    return this.first;
+  }
 
-	public void setFirst(A first) {
-		this.first = first;
-	}
+  public void setFirst(final A first) {
+    this.first = first;
+  }
 
-	public B getSecond() {
-		return second;
-	}
+  public B getSecond() {
+    return this.second;
+  }
 
-	public void setSecond(B second) {
-		this.second = second;
-	}
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((second == null) ? 0 : second.hashCode());
-		return result;
-	}
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if(this == obj){
-			return true;
-		}
-		if(obj == null){
-			return false;
-		}
-		if(!(obj instanceof Pair)){
-			return false;
-		}
-		Pair other = (Pair) obj;
-		if(second == null){
-			if(other.second != null){
-				return false;
-			}
-		}
-		else if(!second.equals(other.second)){
-			return false;
-		}
-		return true;
-	}
+  public void setSecond(final B second) {
+    this.second = second;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((this.second == null) ? 0 : this.second.hashCode());
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(final Object obj) {
+    if(this == obj){
+      return true;
+    }
+    if(obj == null){
+      return false;
+    }
+    if(!(obj instanceof Pair)){
+      return false;
+    }
+    final Pair<?, ?> other = (Pair<?, ?>) obj;
+    if(this.second == null){
+      if(other.second != null){
+        return false;
+      }
+    }
+    else if(!this.second.equals(other.second)){
+      return false;
+    }
+    return true;
+  }
 }
