a3e5221d12933e6d17995fe713b178d95946d7d8
Lukáš Kvídera <kvideral@qwsome.eu>
2019-04-19 09:55:17 +0200

    Cleanup MRU

--- cachesim2/src/main/java/cz/zcu/kiv/cacheSimulator/cachePolicies/MRU.java
+++ cachesim2/src/main/java/cz/zcu/kiv/cacheSimulator/cachePolicies/MRU.java
@@ -8,159 +8,159 @@ import cz.zcu.kiv.cacheSimulator.shared.GlobalVariables;
 
 /**
  * class for MRU algorithm
- * 
+ * <p>
  * SOURCE: Adapted from article "An evaluation of buffer management strategies for relational database systems", by
  * H.-T. Chou and D. J. DeWitt
- *  
- * @author Pavel Bžoch
  *
+ * @author Pavel Bžoch
  */
 public class MRU implements ICache {
 
-	/**
-	 * struktura pro uchovani souboru
-	 */
-	private final ArrayList<FileOnClient> fQueue;
-	
-	/**
-	 * struktura pro ukladani souboru, ktere jsou vetsi nez cache
-	 */
-	private final ArrayList<FileOnClient> fOverCapacity;
-	
+  /**
+   * struktura pro uchovani souboru
+   */
+  private final ArrayList<FileOnClient> fQueue;
 
-	/**
-	 * velikost cache v B
-	 */
-	private long capacity = 0;
+  /**
+   * struktura pro ukladani souboru, ktere jsou vetsi nez cache
+   */
+  private final ArrayList<FileOnClient> fOverCapacity;
 
-	/**
-	 * konstruktor - inicializace cache
-	 */
-	public MRU() {
-    this.fQueue = new ArrayList<FileOnClient>();
-		this.capacity = GlobalVariables.getCacheCapacity();
-		this.fOverCapacity = new ArrayList<FileOnClient>();
-	}
 
-	@Override
+  /**
+   * velikost cache v B
+   */
+  private long capacity;
+
+  /**
+   * konstruktor - inicializace cache
+   */
+  public MRU() {
+    this.fQueue = new ArrayList<>();
+    this.capacity = GlobalVariables.getCacheCapacity();
+    this.fOverCapacity = new ArrayList<>();
+  }
+
+  @Override
   public boolean isInCache(final String fName) {
-		for (final FileOnClient f : this.fQueue) {
-			if (f.getFileName().equalsIgnoreCase(fName)) {
+    for (final FileOnClient f : this.fQueue) {
+      if (f.getFileName().equalsIgnoreCase(fName)) {
         return true;
       }
-		}
-		return false;
-	}
+    }
+    return false;
+  }
 
-	@Override
-	public FileOnClient getFileFromCache(final String fName) {
-		FileOnClient fileForGetting = null;
-		for (final FileOnClient f : this.fQueue) {
-			if (f.getFileName().equalsIgnoreCase(fName)) {
-				fileForGetting = f;
-				break;
-			}
-		}
-		if (fileForGetting == null) {
+  @Override
+  public FileOnClient getFileFromCache(final String fName) {
+    FileOnClient fileForGetting = null;
+    for (final FileOnClient f : this.fQueue) {
+      if (f.getFileName().equalsIgnoreCase(fName)) {
+        fileForGetting = f;
+        break;
+      }
+    }
+    if (fileForGetting == null) {
       return null;
     } else {
       this.fQueue.remove(fileForGetting);
       this.fQueue.add(fileForGetting);
-			return fileForGetting;
-		}
-	}
+      return fileForGetting;
+    }
+  }
 
-	@Override
-	public long freeCapacity() {
-		long obsazeno = 0;
-		for (final FileOnClient f : this.fQueue) {
-			obsazeno += f.getFileSize();
-		}
-		return this.capacity - obsazeno;
-	}
+  @Override
+  public long freeCapacity() {
+    long obsazeno = 0;
+    for (final FileOnClient f : this.fQueue) {
+      obsazeno += f.getFileSize();
+    }
+    return this.capacity - obsazeno;
+  }
 
-	@Override
-	public void removeFile() {
+  @Override
+  public void removeFile() {
     this.fQueue.remove(this.fQueue.size() - 1);
-	}
+  }
 
-	@Override
-	public void insertFile(final FileOnClient f) {
-		//napred zkontrolujeme, jestli se soubor vejde do cache
-		//pokud se nevejde, vztvorime pro nej okenko
-		if (f.getFileSize() > this.capacity){
-			if (!this.fOverCapacity.isEmpty()){
+  @Override
+  public void insertFile(final FileOnClient f) {
+    //napred zkontrolujeme, jestli se soubor vejde do cache
+    //pokud se nevejde, vztvorime pro nej okenko
+    if (f.getFileSize() > this.capacity) {
+      if (!this.fOverCapacity.isEmpty()) {
         this.fOverCapacity.add(f);
-				return;
-			}
-			while (freeCapacity() < (long)((double)this.capacity * GlobalVariables.getCacheCapacityForDownloadWindow())) {
+        return;
+      }
+      while (freeCapacity() < (long) ((double) this.capacity * GlobalVariables.getCacheCapacityForDownloadWindow())) {
         removeFile();
       }
       this.fOverCapacity.add(f);
-			this.capacity = (long) ((double)this.capacity * (1-GlobalVariables.getCacheCapacityForDownloadWindow()));
-			return;
-		}
-		
-		if (!this.fOverCapacity.isEmpty()) {
+      this.capacity = (long) ((double) this.capacity * (1 - GlobalVariables.getCacheCapacityForDownloadWindow()));
+      return;
+    }
+
+    if (!this.fOverCapacity.isEmpty()) {
       checkTimes();
     }
-			
-		//pokud se soubor vejde, fungujeme spravne
-		while (freeCapacity() < f.getFileSize()) {
-			removeFile();
-		}
-    this.fQueue.add(f);
-	}
-	
-	@Override
-	public String toString(){
-		return "MRU";
-	}
-	@Override
-	public boolean needServerStatistics() {
-		return false;
-	}
-	
-	@Override
-	public void setCapacity(final long capacity) {
-		this.capacity = capacity;
-	}
 
-	@Override
-	public void reset() {
-		this.fQueue.clear();
-		this.fOverCapacity.clear();
-	}
-	
-	/**
-	 * metoda pro kontrolu, zda jiz nejsou soubory s vetsi velikosti nez cache stazene - pak odstranime okenko
-	 */
-	private void checkTimes() {
-		boolean hasBeenRemoved = true;
-		while (hasBeenRemoved){
-			hasBeenRemoved = false;
-			if (!this.fOverCapacity.isEmpty() && this.fOverCapacity.get(0).getFRemoveTime() < GlobalVariables.getActualTime()){
+    //pokud se soubor vejde, fungujeme spravne
+    while (freeCapacity() < f.getFileSize()) {
+      removeFile();
+    }
+    this.fQueue.add(f);
+  }
+
+  @Override
+  public String toString() {
+    return "MRU";
+  }
+
+  @Override
+  public boolean needServerStatistics() {
+    return false;
+  }
+
+  @Override
+  public void setCapacity(final long capacity) {
+    this.capacity = capacity;
+  }
+
+  @Override
+  public void reset() {
+    this.fQueue.clear();
+    this.fOverCapacity.clear();
+  }
+
+  /**
+   * metoda pro kontrolu, zda jiz nejsou soubory s vetsi velikosti nez cache stazene - pak odstranime okenko
+   */
+  private void checkTimes() {
+    boolean hasBeenRemoved = true;
+    while (hasBeenRemoved) {
+      hasBeenRemoved = false;
+      if (!this.fOverCapacity.isEmpty() && this.fOverCapacity.get(0).getFRemoveTime() < GlobalVariables.getActualTime()) {
         this.fOverCapacity.remove(0);
-				hasBeenRemoved = true;
-			}
-		}
-		if (this.fOverCapacity.isEmpty()){
-			this.capacity = GlobalVariables.getCacheCapacity();
-		}
-	}
-	
-	@Override
-	public String cacheInfo(){
-		return "MRU;MRU";
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (this.capacity ^ (this.capacity >>> 32));
-		result = prime * result + ((toString() == null) ? 0 : toString().hashCode());
-		return result;
-	}
+        hasBeenRemoved = true;
+      }
+    }
+    if (this.fOverCapacity.isEmpty()) {
+      this.capacity = GlobalVariables.getCacheCapacity();
+    }
+  }
+
+  @Override
+  public String cacheInfo() {
+    return "MRU;MRU";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (int) (this.capacity ^ (this.capacity >>> 32));
+    result = prime * result + ((toString() == null) ? 0 : toString().hashCode());
+    return result;
+  }
 
 }
