4b24c383b0d240abd1a0b522149a536387acee4e
Lukáš Kvídera <kvideral@qwsome.eu>
2019-04-19 10:04:01 +0200

    Cleanup FileOnClient

--- cachesim2/src/main/java/cz/zcu/kiv/cacheSimulator/shared/FileOnClient.java
+++ cachesim2/src/main/java/cz/zcu/kiv/cacheSimulator/shared/FileOnClient.java
@@ -6,129 +6,137 @@ import cz.zcu.kiv.cacheSimulator.server.FileOnServer;
 /**
  * trida pro reprezentaci souboru na strane klienta
  * udrzuje informace nutne pro cachovaci algoritmy
- * 
- * @author Pavel Bzoch
  *
+ * @author Pavel Bzoch
  */
 public class FileOnClient {
 
-	/**
-	 * nazev souboru vcetne cele cesty
-	 */
-	private String fileName;
+  /**
+   * nazev souboru vcetne cele cesty
+   */
+  private final String fileName;
 
-	/**
-	 * velikost souboru v Bytech
-	 */
-	private long fileSize;
+  /**
+   * velikost souboru v Bytech
+   */
+  private final long fileSize;
 
-	/**
-	 * promenne pro pocitani hitu (statistiky na serveru)
-	 */
-	private int readHit = 0, writeHit = 0;
-	
-	/**
-	 * cas, kdy soubor prisel do cache
-	 */
-	private long accessTime = 0;
+  /**
+   * promenne pro pocitani hitu (statistiky na serveru)
+   */
+  private int readHit;
+  private int writeHit;
 
-	/**
-	 * konstruktor - inicializace promennych
-	 * @param fileName
-	 * @param fileSize
-	 * @param accessTime
-	 */
-	public FileOnClient(String fileName, long fileSize, long accessTime) {
-		super();
-		this.fileName = fileName;
-		this.fileSize = fileSize;
-		this.accessTime = accessTime;
-	}
-	
-	/**
-	 * konstruktor - inicializace promennych - ze strany serveru
-	 * @param file
-	 * @param cache
-	 * @param accessTime
-	 */
-	public FileOnClient(FileOnServer file, ICache cache, long accessTime){
-		super();
-		this.fileName = file.getFileName();
-		this.fileSize = file.getFileSize();
-		this.readHit = file.getReadHit(cache.hashCode());
-		this.writeHit = file.getWriteHit(cache.hashCode());
-		this.accessTime = accessTime;
-	}
-	
-	/**
-	 * metoda vraci, kdy je mozne soubor odstranit z cache
-	 * na zaklade prumerne rychlosti site vypocte, jak dlouho bude trvat soubor stahnout
-	 * @return cas mozneho odstraneni
-	 */
-	public long getFRemoveTime(){
-		if (this.accessTime == 0) return 0;
-		return this.fileSize * 8 /1000 / (GlobalVariables.getAverageNetworkSpeed()) + accessTime;
-	}
-	
-	/**
-	 * metoda vraci aktualni cas (cas pristupu k souboru)
-	 * @return caspristupu
-	 */
-	public long getActualTime(){
-		return this.accessTime;
-	}
+  /**
+   * cas, kdy soubor prisel do cache
+   */
+  private final long accessTime;
 
-	/**
-	 * getter pro zjisteni poctu pristupu na cteni souboru
-	 * @return pocet cteni souboru
-	 */
-	public int getReadHit() {
-		return readHit;
-	}
+  /**
+   * konstruktor - inicializace promennych
+   *
+   * @param fileName   the name of file
+   * @param fileSize   the size of file
+   * @param accessTime last access time
+   */
+  public FileOnClient(final String fileName, final long fileSize, final long accessTime) {
+    this.fileName = fileName;
+    this.fileSize = fileSize;
+    this.accessTime = accessTime;
+  }
 
-	/**
-	 * metoda pro zvyseni poctu pristupu na cteni
-	 */
-	public void increaseReadHit() {
-		this.readHit++;
-	}
+  /**
+   * konstruktor - inicializace promennych - ze strany serveru
+   *
+   * @param file
+   * @param cache
+   * @param accessTime
+   */
+  public FileOnClient(final FileOnServer file, final ICache cache, final long accessTime) {
+    this.fileName = file.getFileName();
+    this.fileSize = file.getFileSize();
+    this.readHit = file.getReadHit(cache.hashCode());
+    this.writeHit = file.getWriteHit(cache.hashCode());
+    this.accessTime = accessTime;
+  }
 
-	/**
-	 * getter pro zjisteni poctu pristupu na zapis do souboru
-	 * @return pocet zapisu do souboru
-	 */
-	public int getWriteHit() {
-		return writeHit;
-	}
+  /**
+   * metoda vraci, kdy je mozne soubor odstranit z cache
+   * na zaklade prumerne rychlosti site vypocte, jak dlouho bude trvat soubor stahnout
+   *
+   * @return cas mozneho odstraneni
+   */
+  public long getFRemoveTime() {
+    if (this.accessTime == 0) {
+      return 0;
+    }
+    return this.fileSize * 8 / 1000 / (GlobalVariables.getAverageNetworkSpeed()) + this.accessTime;
+  }
 
-	/**
-	 * metoda pro zvyseni poctu pristupu na zapis
-	 */
-	public void increaseWriteHit() {
-		this.writeHit++;
-	}
+  /**
+   * metoda vraci aktualni cas (cas pristupu k souboru)
+   *
+   * @return caspristupu
+   */
+  public long getActualTime() {
+    return this.accessTime;
+  }
 
-	/**
-	 * metoda pro zjisteni jmena souboru
-	 * @return jmeno souboru
-	 */
-	public String getFileName() {
-		return fileName;
-	}
+  /**
+   * getter pro zjisteni poctu pristupu na cteni souboru
+   *
+   * @return pocet cteni souboru
+   */
+  public int getReadHit() {
+    return this.readHit;
+  }
 
-	/**
-	 * metoda pro zjisteni velikosti souboru
-	 * @return velikost souboru
-	 */
-	public long getFileSize() {
-		return fileSize;
-	}
+  /**
+   * metoda pro zvyseni poctu pristupu na cteni
+   */
+  public void increaseReadHit() {
+    this.readHit++;
+  }
 
-	/**
-	 * metoda por vyresetovani citacu
-	 */
-	public void resetCounters(){
-		this.readHit = 0;
-		this.writeHit = 0;
-	}
+  /**
+   * getter pro zjisteni poctu pristupu na zapis do souboru
+   *
+   * @return pocet zapisu do souboru
+   */
+  public int getWriteHit() {
+    return this.writeHit;
+  }
+
+  /**
+   * metoda pro zvyseni poctu pristupu na zapis
+   */
+  public void increaseWriteHit() {
+    this.writeHit++;
+  }
+
+  /**
+   * metoda pro zjisteni jmena souboru
+   *
+   * @return jmeno souboru
+   */
+  public String getFileName() {
+    return this.fileName;
+  }
+
+  /**
+   * metoda pro zjisteni velikosti souboru
+   *
+   * @return velikost souboru
+   */
+  public long getFileSize() {
+    return this.fileSize;
+  }
+
+  /**
+   * metoda por vyresetovani citacu
+   */
+  public void resetCounters() {
+    this.readHit = 0;
+    this.writeHit = 0;
+  }
 }
